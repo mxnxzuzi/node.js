23ad763d28cfee054b3326c59bd751170c10067d
minju <iaki111@naver.com>
2024-03-28 17:58:55 +0900

    [chapter5]

---
+++ chapter5/callback-promise-async-await/promise-test.js
@@ -0,0 +1,41 @@
+const DB = [];
+
+function saveDB(user) {
+    const oldDBSize = DB.length + 1;
+    DB.push(user);
+    console.log(`save ${user.name} to DB`);
+    return new Promise((resolve, reject) => {
+        if(DB.length > oldDBSize){
+            resolve(user);
+        } else {
+            reject(new Error("Save DB Error!"));
+        }
+    });
+}
+
+function sendEmail(user, callback) {
+    console.log(`email to ${user.email}`);
+    return new Promise((resolve) => {
+        resolve(user);
+    });
+}
+
+function getResult(user) {
+    return new Promise((resolve, reject) => {
+        resolve(`success register ${user.name}`);
+    });
+}
+
+function registerByPromise(user) {
+    // 비동기지만 순서 지켜 실행
+    const result = saveDB(user).then(sendEmail).then(getResult).catch(error => new Error(error)).finally(() => console.log("완료!"));
+    console.log(result);
+    return result;
+}
+
+const myUser = {email : "minju@test.com", password: "1234", name: "minju"};
+const result = registerByPromise(myUser);
+result.then(console.log);
+
+//allResult = Promise.all([saveDB(myUser), sendEmail(myUser), getResult(myUser)]);
+//allResult.then(console.log);
